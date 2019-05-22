dd6804347004cf4603e596dd6ce47111646e3e4d
dqisme <dqisme@outlook.com>
2019-05-22 16:00:03 +0800

    [dq] implement login checking.

--- src/index.tsx
+++ src/index.tsx
@@ -4,3 +4,9 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+window.chrome.cookies.getAll({ name: 'centralauth_User' }, ([cookie]) => {
+  if (!cookie) {
+    window.chrome.tabs.create({ url: 'https://en.wikipedia.org/w/index.php?title=Special:UserLogin&returnto=Main+Page' })
+  }
+});
